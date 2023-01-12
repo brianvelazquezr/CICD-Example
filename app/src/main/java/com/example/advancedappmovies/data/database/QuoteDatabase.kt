da4e3851d1a19175377bb139fc087d1d84048200
BVR <brian.extrastuff@hotmail.com>
2023-01-12 16:50:33 -0600

    CI fixed 4

--- app/src/main/java/com/example/advancedappmovies/data/database/QuoteDatabase.kt
+++ app/src/main/java/com/example/advancedappmovies/data/database/QuoteDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.advancedappmovies.data.database.dao.QuoteDao
 import com.example.advancedappmovies.data.database.entities.QuoteEntity
 
-@Database(entities = [QuoteEntity::class], version = 1)
+@Database(entities = [QuoteEntity::class], version = 1, exportSchema = false)
 abstract class QuoteDatabase: RoomDatabase() {
 
     abstract fun getQuoteDao(): QuoteDao
